a0eee4ebbd16a7f4a71ef80a1e2be4f2f0de981f
Vijayavedhasekarn <vedhatech02@gmail.com>
2024-01-07 22:11:02 +0530

    design: design ui for weather app and setup js

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,16 @@
-console.log("hi");
+const apiKey = "f7055b8d8cb767d8652bd2f00417fc43";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=vedaranyam";
+
+function getWeather() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let responseData = JSON.parse(this.response);
+      console.log(responseData);
+    }
+  };
+  xhttp.open("GET", `${apiUrl}&appid=${apiKey}`, true);
+  xhttp.send();
+}
+getWeather();
